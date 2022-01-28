dbed515a8c7729514994438d5babe77ab2c95945
Jason Leung <leungj@PRISM-M93P-2.PRISMLAB>
2022-01-28 11:44:00 -0500

    updated channel names and processing script

--- run_brainflow_lsl.py
+++ run_brainflow_lsl.py
@@ -4,7 +4,7 @@ board_id = 2
 serial_port = 'COM3'
 name = 'mindset_EXG'
 data_type = 'EXG'
-channel_names = 'FP1,FP2,C3,C4,P7,P8,O1,O2,F7,F8,F3,F4,T7,T8,P3,P4'
+channel_names = 'Fz,FC1,FC2,C3,Cz,C4,CP1,CP2,P3,Pz,P4,PO3,PO4,O1,Oz,O2'
 uid = 'brainflow'
 
 cmd = "python brainflow_lsl.py --board-id %s --serial-port %s --name %s --data-type %s --channel-names %s --uid %s" % (board_id, serial_port, name, data_type, channel_names, uid)
